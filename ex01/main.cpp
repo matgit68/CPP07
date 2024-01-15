b1524b705881d23e737fd6fbf58968f5957535ac
mdjemaa <mdjemaa@student.42mulhouse.fr>
2024-01-15 18:44:13 +0100

    evalmain

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,7 +5,7 @@ int main(void) {
     std::string strTab[5] = {"Premier","Deuxième","Troisième","Quatrième","Cinquième"};
 
     int intTab[4] = {10, 20, 30, 40};
-    iter(strTab, 5, &disp);
-    iter(intTab, 4, &disp);
-    iter(intTab, 4, &square);
+    iter(strTab, 5, disp<std::string>);
+    iter(intTab, 4, disp<int>);
+    iter(intTab, 4, square<int>);
 }
